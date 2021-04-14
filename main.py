1d7396e7009c0eccaf8f2a35d5b9ac4b6359c8fc
Antoine <AntOlivero@users.noreply.github.com>
2021-04-15 01:21:51 +0200

    honk
    honk very well

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
-def main()
-    hello = "hello"
-    print(hello)
+from playsound import playsound
+from pynput.keyboard import Listener
+import threading
+
+def honk() :
+    while True :
+        on_press(key)
+
+
+class myThread(threading.Thread) :
+    def __init__(self, name) :
+        threading.Thread.__init__(self)
+        self.name = name
+    def run(self) :
+        honk()
+
+def on_press(key) :
+    keyP = "{0}".format(key)
+    if(keyP == '\'h\'') :
+        playsound("honk-sound.mp3")
+
+def main() :
+    with Listener(on_press) as listener :
+        listener.join()
+    honkThread = myThread("honk")
+    honkThread.start()
+
+
+if __name__ == "__main__" :
+    main()
